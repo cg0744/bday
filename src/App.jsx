59dd811107b5c1c5fe9ae5ba74ac791e0c49ec2b
chrisG074 <1355639@student.rocvantwente.nl>
2026-04-04 00:05:53 +0200

    correctly added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SectionWrapper from './components/SectionWrapper';
 import Section1 from './components/sections/Section1';
 import Section2 from './components/sections/Section2';
 import Section3 from './components/sections/Section3';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [unlockedSection, setUnlockedSection] = useState(1);
